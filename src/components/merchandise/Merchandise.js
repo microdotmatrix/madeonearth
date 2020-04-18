c9ee7c8f6da5ed2aa0896ebd3e1597c35369aeb2
Stanley Chen <dannyshien@gmail.com>
2020-04-18 14:13:35 -0400

    Setup base layout for site

---
+++ src/components/merchandise/Merchandise.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Products from '../products/Products';
+
+
+class Merchandise extends Component {
+
+  render() {
+    return (
+      <div>
+        <Products />
+      </div>
+    )
+  }
+}
+
+export default Merchandise;
