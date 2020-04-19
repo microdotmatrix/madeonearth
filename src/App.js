9a527fde076c08aa0f4390e950e681c57e045001
Stanley Chen <dannyshien@gmail.com>
2020-04-19 12:42:45 -0400

    Add shopify react-js-buy components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-import Merchandise from './components/merchandise/Merchandise';
+import Products from './components/products/Products';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Made on Earth</h1>
-      </header>
-      <Merchandise />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+  }
+
+  // componentDidMount() {
+  //   this.props.client.product.fetchAll().then((res) => {
+  //     console.log(res)
+  //   })
+  // }
+  render() {
+    // const client = this.props.client
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Made on Earth</h1>
+        </header>
+        <Products />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Client from 'shopify-buy';
 import * as serviceWorker from './serviceWorker';
 
 const client = Client.buildClient({
   domain: 'https://madeonearthapparel.myshopify.com/',
-  storefrontAccessToken: 'ACCESS_TOKEN'
+  storefrontAccessToken: `${process.env.REACT_APP_ACCESS_TOKEN}`
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App client={ client } />,
   document.getElementById('root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
