fe76d24524bd7858d9491c5ad5c612008991efc3
Stanley Chen <dannyshien@gmail.com>
2020-07-23 13:44:22 -0400

    Center image with flexbox and 100vh/vw

--- src/components/underConstruction/UnderConstruction.js
+++ src/components/underConstruction/UnderConstruction.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'; 
 import SiteDownImg from '../../asset/UC.png';
+import './UnderConstruction.css';
 
 class UnderConstruction extends Component {
   render() {
     return (
-      <div>
-        <img className='constructionImg' src={ SiteDownImg } alt='MadeonEarth is currently under construction' />
+      <div className='siteDown'>
+        <img className='siteDownIMG' src={ SiteDownImg } alt='MadeonEarth is currently under construction' />
       </div>
     )
   }  
