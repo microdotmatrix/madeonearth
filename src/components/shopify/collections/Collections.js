f4dcfa5ef4af00ad281949c296127713bad98d97
Stanley Chen <dannyshien@gmail.com>
2020-08-08 11:00:02 -0400

    Rename files from Product.js to ProductPage.js

--- src/components/shopify/collections/Collections.js
+++ src/components/shopify/collections/Collections.js
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
 import './Collections.css';
-import Product from '../product/Product';
+import ProductPage from '../productPage/ProductPage';
 
 class Collections extends Component {
 
@@ -9,7 +9,7 @@ class Collections extends Component {
     if (this.props.collections) {
       collections = this.props.collections[0].products.map((product) => {
         return (
-          <Product 
+          <ProductPage 
             addVariantToCart={ this.props.addVariantToCart }
             client={ this.props.client }
             key={ product.id.toString() }
