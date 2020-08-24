dd41e7121f3f7926b6ea1a627b9023669a335ae0
Stanley Chen <dannyshien@gmail.com>
2020-08-23 21:12:34 -0400

    Move LINK around productImage and productTitle

--- src/components/shopify/product/Product.js
+++ src/components/shopify/product/Product.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './Product.css';
 
+import { Link } from 'react-router-dom';
+
 const Product = ({ product }) => {
+  let id = product.id
   let variantImage = product.images[0]
   return (
       <div className='product'>
-        <img 
-          src={ variantImage.src }
-          alt={ `${product.title} product image` }
-        />
-        <h3 className='productTitle'>{ product.title }</h3>
+        <Link 
+          className='imageLink'
+          to={{ pathname: `/productpage/${ id }` }}
+        >
+          <img 
+            src={ variantImage.src }
+            alt={ `${product.title} product image` }
+          />
+        </Link>
+
+        <Link
+          className='titleLink'
+          to={{ pathname: `/productpage/${ id }` }}
+        >
+          <h3 className='productTitle'>{ product.title }</h3>
+        </Link>
       </div>
   )
 }
