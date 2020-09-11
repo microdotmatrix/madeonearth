e9781d12da9720c161fdfe803f655f30180f5f1e
Stanley Chen <dannyshien@gmail.com>
2020-09-11 14:51:01 -0400

    Remove Lazy from react

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MadeOnEarth from '../components/madeonearth/MadeOnEarth';
 
