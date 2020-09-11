126ae441561b465106840d4f2c7c58dce2010aaf
Stanley Chen <dannyshien@gmail.com>
2020-09-11 11:36:23 -0400

    add addVariantToCart method

--- src/components/madeonearth/MadeOnEarth.js
+++ src/components/madeonearth/MadeOnEarth.js
@@ -65,6 +65,16 @@ class MadeOnEarth extends Component {
       store.dispatch({ type: 'SHOP_FOUND', payload: res})
     });
   }
+
+  addVariantToCart = (variantId, quantity) => {
+    const state = store.getState();
+    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }]
+    const checkoutId = state.checkout.id
+    state.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
+      store.dispatch({type: 'ADD_VARIANT_TO_CART', payload: { isCartOpen: true, checkout: res }});
+      sessionStorage.setItem('cartItems', JSON.stringify(res))
+    });
+  }
  
   updateQuantityInCart = (lineItemId, quantity) => {
     const state = store.getState();
