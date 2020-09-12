dd6528ff255c0a7de44ca8332a344afa92c52b2e
Stanley Chen <dannyshien@gmail.com>
2020-09-12 12:12:59 -0400

    Revert these commits?

--- src/components/shopify/collections/Collections.js
+++ src/components/shopify/collections/Collections.js
@@ -6,65 +6,24 @@ import { connect } from 'react-redux';
 import store from '../../../store/Store';
 
 class Collections extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNotification: false,
-      productName: '',
-      variantSize: ''
-    }
-  }
-
-  addVariantToCart = (product, variant, quantity) => {
-
-    const state = store.getState();
-    let variantId = variant.id
-    let productName = product.title;
-    let variantSize = variant.title;
-    const lineItemsToAdd = [{ variantId, quantity: parseInt(quantity, 10) }]
-    const checkoutId = state.checkout.id
-    state.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
-      store.dispatch({type: 'ADD_VARIANT_TO_CART', payload: { checkout: res }});
-      sessionStorage.setItem('cartItems', JSON.stringify(res))
-      this.setState({
-        isNotification: true,
-        productName,
-        variantSize
-      })
-    });
-    this.clearNotification();
-  }
-
-  clearNotification = () => {
-    if (!this.state.isNotification) {
-      setTimeout(() => {
-        this.setState({
-          isNotification: false
-        })
-      }, 4000)
-    };
-  }
 
   render() {
     const state = store.getState()
 
     let products;
-    if (state.collections) {
-      products = state.collections[0].products.map((product, i) => {
-        let id = product.id
+    // if (state.collections) {
+    //   products = state.collections[0].products.map((product, i) => {
+    //     let id = product.id
 
     // ============= DELETE ==================
-    // if (state.products) {
-    //   products = state.products.map(product => {
-    //     let id = product.id
+    if (state.products) {
+      products = state.products.map(product => {
+        let id = product.id
     // =======================================
     
         return (
           <Product 
-            addVariantToCart={ this.addVariantToCart }
-            notification={ this.state.isNotification }
-            productName={ this.state.productName }
-            variantSize={ this.state.variantSize }
+            addVariantToCart={ this.props.addVariantToCart }
             client={ state.client }
             key={ id }
             product={ product }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import MadeOnEarth from '../components/madeonearth/MadeOnEarth';
 
 
-// const ProductPage = lazy((() => import('../components/productPage/ProductPage')));
+const ProductPage = lazy((() => import('../components/productPage/ProductPage')));
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path ='/' component={ MadeOnEarth } />
-      {/* <Route exact path ='/productpage/:productId' render={(props) => <ProductPage {...props} />} /> */}
+      <Route exact path ='/productpage/:productId' render={(props) => <ProductPage {...props} />} />
     </Switch>
   )
 };
